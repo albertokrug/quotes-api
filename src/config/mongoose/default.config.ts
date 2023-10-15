cca8dbb6327f76e8c97a8d08cd5eb9fad7ff0d28
albertokrug <albertkrug@icloud.com>
2023-10-15 21:45:40 +0200

    first version

--- src/config/mongoose/default.config.ts
+++ src/config/mongoose/default.config.ts
@@ -1,6 +1,12 @@
+import { isProduction } from "../envs/index";
+
+let connectionUrl = "mongodb://127.0.0.1:27017/";
+if (isProduction) {
+  connectionUrl = "mongodb+srv://albertkrug:4lf3mDEvtxAA34I4@cluster0.0yvqkam.mongodb.net/quotes?retryWrites=true&w=majority";
+}
+
 export default {
   id: "quotes",
-  url:
-    process.env.DEFAULT_URL || "mongodb+srv://albertkrug:4lf3mDEvtxAA34I4@cluster0.0yvqkam.mongodb.net/quotes?retryWrites=true&w=majority",
+  url: process.env.DEFAULT_URL || connectionUrl,
   connectionOptions: {}
 };
