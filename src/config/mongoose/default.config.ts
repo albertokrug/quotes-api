6c8812dbdd96a39094aea7606249da9494f26171
albertokrug <albertkrug@icloud.com>
2023-10-16 20:14:15 +0200

    first version

--- src/config/mongoose/default.config.ts
+++ src/config/mongoose/default.config.ts
@@ -2,7 +2,7 @@ import { isProduction } from "../envs/index";
 
 let connectionUrl = "mongodb://127.0.0.1:27017/";
 if (isProduction) {
-  connectionUrl = process.env.MONGODB_URI;
+  connectionUrl = process.env.MONGODB_URI as string;
 }
 
 export default {
